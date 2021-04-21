097910572924ba1b66967e99be277e46cf8f2a52
avrcoelho <andrevrcoelho@hotmail.com>
2021-04-20 21:42:43 -0300

    feat: pages and navigation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,27 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Welcome } from "./src/pages/Welcome";
+import { StatusBar } from "expo-status-bar";
+import {
+  useFonts,
+  Jost_400Regular,
+  Jost_600SemiBold,
+} from "@expo-google-fonts/jost";
+import AppLoading from "expo-app-loading";
+
+import { Routes } from "./src/routes";
 
 export default function App() {
+  const [fonstLoaded] = useFonts({
+    Jost_400Regular,
+    Jost_600SemiBold,
+  });
+
+  if (!fonstLoaded) {
+    return <AppLoading />;
+  }
+
   return (
     <>
-      <Welcome />
+      <Routes />
       <StatusBar style="auto" />
     </>
   );
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import {
-  TouchableOpacity,
   Text,
+  TouchableOpacity,
   StyleSheet,
   TouchableOpacityProps,
 } from "react-native";
-
 import colors from "../../styles/colors";
 
 interface ButtonProps extends TouchableOpacityProps {
@@ -14,24 +13,22 @@ interface ButtonProps extends TouchableOpacityProps {
 
 export const Button = ({ title, ...rest }: ButtonProps) => {
   return (
-    <TouchableOpacity style={styles.button} activeOpacity={0.7} {...rest}>
-      <Text style={styles.buttonText}>{title}</Text>
+    <TouchableOpacity style={styles.container} {...rest}>
+      <Text style={styles.text}>{title}</Text>
     </TouchableOpacity>
   );
 };
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
+    height: 56,
+    borderRadius: 16,
     justifyContent: "center",
     alignItems: "center",
-    borderRadius: 16,
-    marginBottom: 10,
-    height: 56,
-    width: 56,
   },
-  buttonText: {
+  text: {
+    fontSize: 16,
     color: colors.white,
-    fontSize: 24,
   },
 });
